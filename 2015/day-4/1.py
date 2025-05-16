41e0ceb3474a60702e02a0f0d6a3f500da291db4
Raghav Sharma <code.practice17@gmail.com>
2025-05-16 21:51:37 +0530

    Added Day 4

---
+++ 2015/day-4/1.py
@@ -0,0 +1,30 @@
+import hashlib
+
+key = "yzbqklnj"
+num = 0
+
+
+def is_zero(sliced_string):
+    for i in range(0, 5):
+        if sliced_string[i] != "0":
+            return False
+    
+    return True
+
+
+while True:
+    string = key
+    
+    string += str(num)
+
+    encoded = string.encode()
+    hashed_string = hashlib.md5(encoded).hexdigest()
+    
+    sliced_string = hashed_string[0: 5]
+
+    if(is_zero(sliced_string)):
+        break
+    
+    num += 1
+
+print(num)
